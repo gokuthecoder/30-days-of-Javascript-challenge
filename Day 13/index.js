6923a581693322e9bcdc94515c7ee25e9d92d0ee
gokuthecoder <vishalkr12012001@gmail.com>
2024-07-25 11:39:31 +0530

    chore: Remove unused module and package.json file

--- Day 13/index.js
+++
@@ -1,3 +0,0 @@
-import { summation as sum} from "./module.js";
-
-console.log(sum(Infinity,23));
--- Day 13/module.js
+++
@@ -1,8 +0,0 @@
-export const summation = (num1, num2) =>{
-    if (typeof num1 == "number" && typeof num2 == "number") {
-        const sum = num1 + num2;
-        return sum;
-    } else {
-        return new Error("number1 and number2 are not a valid number")
-    }
-};
